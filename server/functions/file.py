2b794866e2be0aca0443ba0732887a5decb786d7
davidxx04 <152333556+davidxx04@users.noreply.github.com>
2024-10-13 16:16:00 +0200

    file.py

--- server/functions/file.py
+++ server/functions/file.py
@@ -1,6 +1,21 @@
-import database
+from functions import database as db 
 
 class File:
-    def __init__(self, userId):
-        database.get_data("users", {"userId": userId})
+    def __init__(self, fileId):
+        fileData = db.get_data( "files", { "fileId": fileId })
+        
+        self.fileId          = fileData[0]
+        self.userId          = fileData[1]
+        self.fileName        = fileData[2]
+        self.encryptedFile   = fileData[3]
+        self.aesKey          = fileData[4]
+        self.publicRSA       = fileData[5]
+        self.privateRSA      = fileData[6]
+        self.date            = fileData[7]
+        self.fileType        = fileData[8]
 
+    def __str__( self ):
+        return f'{{"fileId": "{self.fileId}", "userId": "{self.userId}","fileName": "{self.fileName}", "encryptedFile": "{self.encryptedFile}", "aesKey": "{self.aesKey}", "publicRSA": "{self.publicRSA}", "privateRSA": "{self.privateRSA}", "date": "{self.date}", "fileType": "{self.fileType}"}}'
+    
+    def __repr__( self ):
+        return f'{{"fileId": "{self.fileId}", "userId": "{self.userId}","fileName": "{self.fileName}", "encryptedFile": "{self.encryptedFile}", "aesKey": "{self.aesKey}", "publicRSA": "{self.publicRSA}", "privateRSA": "{self.privateRSA}", "date": "{self.date}", "fileType": "{self.fileType}"}}'
