6a78bd70aef5fa08a7202b35dbba32d7d85b45f8
Marcos <marcosruizrubio@gmail.com>
2024-10-16 18:15:22 +0200

    Corregido y completado login de Google

--- client/functions/google_auth.py
+++ client/functions/google_auth.py
@@ -1,5 +1,7 @@
+import json
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+import requests
 
 from google_user import GoogleUser
 
@@ -9,6 +11,7 @@ flow = InstalledAppFlow.from_client_secrets_file(
 
 # Abre un navegador para que el usuario elija la cuenta de google con la que autenticarse
 # y devuelve un objeto GoogleUser con la info del usuario autenticado
+# Se puede usar para obtener el email y nombre del usuario al registrarlo
 def get_google_user() -> GoogleUser:
     flow.run_local_server()
     credentials = flow.credentials
@@ -17,3 +20,12 @@ def get_google_user() -> GoogleUser:
     user_info = user_info_service.userinfo().get().execute()
 
     return GoogleUser.from_dict(user_info)
+
+# Abre un navegador para que el usuario elija la cuenta de google con la que autenticarse,
+# llama al servicio de login de google y devuelve el resultado (userId y privateRSA)
+def google_login():
+    flow.run_local_server()
+    id_token = flow.credentials.id_token
+    
+    loginGoogle = requests.post('http://localhost:5000/users/login-google', json={"idToken": id_token})
+    return json.loads(loginGoogle.text)
